ad2c2cf6a96208e6c6d0b36f4eecb364a450d400
james <james@mercstudio.com>
2024-12-09 00:38:47 +0800

    captcha fix

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import asyncio
 
 async def main():
     agent = Agent(
-        task="find all available primary school nearby kinrara mas apartment, and get their reviews with proos and cons and state if its private or public school, and also get their address and contact number, and price list. get at least 5 schools",
+        task="find all available primary school nearby kinrara mas apartment, and get their reviews with proos and cons and state if its private or public school, and also get their address and contact number, and price list. get at least 5 schools. if you detect any captcha on screen, pause for 10 seconds and try again.",
         llm=ChatOpenAI(model="gpt-4o-mini"),
         browser=Browser(BrowserConfig(headless=False)),
     )
@@ -14,5 +14,7 @@ async def main():
     print("-------------")
     print(result)
     print("-------------")
+    print("ended.")
+    await agent.browser.close()
 
 asyncio.run(main())
